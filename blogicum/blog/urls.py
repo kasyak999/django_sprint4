c8143283f4fadee8d0946bcf7521d6060fcf3031
Maksim <kasyak999@yandex.ru>
2024-07-10 01:01:57 +0300

    up

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -6,10 +6,10 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.IndexListView.as_view(), name='index'),
     path('posts/<int:pk>/', views.post_detail, name='post_detail'),
-    # path(
-    #     'category/<slug:category_slug>/',
-    #     views.category_posts, name='category_posts'
-    # ),
+    path(
+        'posts/<int:post_id>/edit/', views.PostUpdateView.as_view(), 
+        name='edit_post'
+    ),
     path(
         'category/<slug:category_slug>/',
         views.CategoryPostsListView.as_view(), name='category_posts'
@@ -19,6 +19,7 @@ urlpatterns = [
         name='profile'
     ),
     path(
-        'edit', views.ProfileUpdateView.as_view(), name='edit_profile'
+        'edit/', views.ProfileUpdateView.as_view(), name='edit_profile'
     ),
+    path('posts/create/', views.CreateCreateView.as_view(), name='create_post')
 ]
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,24 +1,55 @@
-from django.shortcuts import render, get_object_or_404
+from django.db.models.query import QuerySet
+from django.shortcuts import render, get_object_or_404, redirect
 # get_list_or_404
 from django.http import HttpResponse, HttpRequest
 from blog.models import Post, Category, User
 from django.utils import timezone
 
-from django.views.generic import DetailView, UpdateView, ListView
+from django.views.generic import DetailView, UpdateView, ListView, CreateView
 from django.urls import reverse_lazy
+
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 class ProfileDetailView(DetailView):
     """Просмотреть профиль пользователя"""
+
     model = User
     template_name = 'blog/profile.html'
     slug_field = 'username'  # Используйте username вместо pk
     slug_url_kwarg = 'username'  # Соответствующее имя в URL
     context_object_name = 'profile'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        if self.request.user.is_authenticated:
+            user_posts = Post.objects.filter(
+                Q(
+                    is_published=True, category__is_published=True,
+                    pub_date__lt=timezone.now(), author=self.object
+                ) | Q(author=self.request.user)
+            ).select_related('location', 'category', 'author')
+        else:
+            user_posts = Post.objects.filter(
+                is_published=True,
+                category__is_published=True,
+                pub_date__lt=timezone.now(),
+                author_id=self.object.id
+            ).select_related('location', 'category', 'author')
+
+        paginator = Paginator(user_posts, self.paginate_by)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        context = super().get_context_data(**kwargs)
+        context['page_obj'] = page_obj
+        return context
 
 
 class ProfileUpdateView(UpdateView):
     """Изменение профиля пользователя"""
+
     model = User
     fields = (
         'username', 'first_name', 'last_name', 'email'
@@ -27,15 +58,41 @@ class ProfileUpdateView(UpdateView):
     success_url = reverse_lazy('blog:index')
 
     def get_object(self):
-        return self.request.user  # Получаем текущего пользователя
+        return self.request.user
 
 
 class IndexListView(ListView):
     """Главная страница"""
+
     queryset = Post.objects.main_filter()
     template_name = 'blog/index.html'
     paginate_by = 10
     context_object_name = 'post_list'
+
+
+class CreateCreateView(CreateView):
+    """Создание нового поста"""
+
+    model = Post
+    fields = (
+        'title', 'text', 'pub_date', 'category', 'location'
+    )
+    template_name = 'blog/create.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        """Только для авторизированых"""
+        if not request.user.is_authenticated:
+            return redirect('login')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse_lazy(
+            'blog:profile', kwargs={'username': self.request.user.username}
+        )
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
@@ -46,8 +103,33 @@ def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
     return render(request, 'blog/detail.html', {'post': post})
 
 
+class PostUpdateView(UpdateView):
+    """Изменение поста пользователя"""
+
+    model = Post
+    fields = (
+        'title', 'text', 'pub_date', 'category', 'location'
+    )
+    template_name = 'blog/user.html'
+    pk_url_kwarg = 'post_id'
+
+    def get_queryset(self):
+        return super().get_queryset().filter(
+            pk=self.kwargs['post_id'], author=self.request.user
+        )
+
+    def get_success_url(self):
+        return reverse_lazy(
+            'blog:post_detail', kwargs={'pk': self.kwargs['post_id']}
+        )
+
+    # def handle_no_permission(self):
+    #     return redirect('blog:index')
+
+
 class CategoryPostsListView(ListView):
     """Вывод постов в категории"""
+
     context_object_name = 'post_list'
     template_name = 'blog/category.html'
     paginate_by = 10
